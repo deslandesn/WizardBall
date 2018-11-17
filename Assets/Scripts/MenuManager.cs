c5910348aa9a73f808a5d3d846f8fc444569ca02
Reg Robertson <reg.robertson1@gmail.com>
2018-11-17 09:46:01 -0500

    button color indications

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
@@ -11,9 +12,18 @@ public class MenuManager : MonoBehaviour
         SceneManager.LoadScene(scene);
     }
 
+
+    Color selected = Color.red;
+    Color available = Color.black;
+    UnityEngine.UI.Button btn = null;
+
+    readonly string[] teams = new string[] { "Red", "Blue" };
+    readonly string[] positions = { "Defence", "Mid", "Forward" };
+
     public void SetPlayerTeam(string value)
     {
         PlayerDataCarrier.instance.SetTeam(value);
+        ToggleButton(teams, value);
         CheckSettings();
     }
 
@@ -21,6 +31,7 @@ public class MenuManager : MonoBehaviour
     {
 
         PlayerDataCarrier.instance.SetPosition(value);
+        ToggleButton(positions, value);
         CheckSettings();
 
     }
@@ -30,12 +41,36 @@ public class MenuManager : MonoBehaviour
         Application.Quit();
     }
 
+    //Check for user team and position
     public void CheckSettings()
     {
         if (PlayerDataCarrier.instance.hasPosition && PlayerDataCarrier.instance.hasTeam)
         {
             UnityEngine.UI.Button button = GameObject.Find("StartGameButton").GetComponent<UnityEngine.UI.Button>();
             button.interactable = true;
+        }
+    }
+
+    //Toggle button group colors to show selected (red) vs available (black)
+    public void ToggleButton(string [] btnGroup, string btnValue)
+    {
+        foreach (string item in btnGroup)
+        {
+            btn = GameObject.Find(item).GetComponent<UnityEngine.UI.Button>();
+            ColorBlock cb = btn.colors;
+
+            if (item == btnValue)
+            {
+                cb.normalColor = selected;
+                cb.pressedColor = selected;
+            }
+            else
+            {
+                cb.normalColor = available;
+                cb.pressedColor = available;
+            }
+
+            btn.colors = cb;
         }
     }
 }
